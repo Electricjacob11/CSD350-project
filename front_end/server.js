1804cb02357d251e62b6fcd14861da506bb9e902
Electricjacob11 <electricjacob11@gmail.com>
2026-05-05 17:00:47 -0700

    Fix

--- front_end/server.js
+++ front_end/server.js
@@ -3,6 +3,9 @@ const mysql = require("mysql2");
 const cors = require("cors");
 
 const app = express();
+const path = require("path");
+
+app.use(express.static(path.join(__dirname, "front end")));
 
 app.use(cors());
 app.use(express.json());
